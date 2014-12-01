98d839c0cbb300c9a44290ef918aaa32ef008cb1
dn222em <dn222em@student.lnu.se>
2014-12-01 12:34:26 +0000

    ?

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -1,6 +1,60 @@
-/*"use strict";
+"use strict";
 
-function Message (message, date){
+/* Denna "klass" skapar vi Message-objekt utifrån.
+ * Vi kapslar in privata variabler med getters och setters. 
+*/
+    function Message (message, date){
+    
+        this.getText = function(){ return this.message; };
+        this.setText = function(_message){
+            if(typeof _message ==="undefined" || _message.trim() ===""){
+                console.log("Textarean är tom!");
+            }
+            this.message = _message;};
+    
+        this.getNewDate = function(){ return this.date;};
+        this.setNewDate = function(_date){ this.date = _date;};
+    }
+    
+    Message.prototype.toString = function (){
+        return this.getText() +"<br/>"+"<br/>" +" (" + this.getNewDate() + ") ";
+    };
+    
+    
+/* function Message (message, date){
+        
+        this.message;
+        this.date;
+    }
+        
+    Message.prototype.getText = function(){
+        return this.message;
+    };
+    
+    Message.prototype.setText = function(newText){
+        
+        if(typeof newText ==="undefined" || newText.trim() ===""){
+            
+            console.log("Textarean är tom!");
+        }
+        
+        this.message = newText; 
+    };
+    
+     Message.prototype.getDate = function(){
+        return this.date;
+    };
+    Message.prototype.setDate = function(newDate){
+        
+       this.date = newDate; 
+    };
+    
+    Message.prototype.toString = function (){
+        
+        return this.getText() +"<br/>"+"<br/>" +" (" + this.getDate() + ") ";
+    };
+
+/*function Message (message, date){
     
     this.getText = function () {return message;};
     this.setText = function (_text) { message = _text;};
--- 2-labbymezzage/init.js
+++ 2-labbymezzage/init.js
@@ -8,7 +8,7 @@
 window.onload = function(){
     
     
-    var now = new Date().toLocaleString();
+    var now = new Date();//.toLocaleString();
     var publish = document.getElementById("publish-button");
     publish.onclick = createMessage;
     
@@ -17,6 +17,7 @@ window.onload = function(){
 	var messages = [];
 	var ul = null;//document.createElement("ul");
 	var li = null;//document.createElement("li");
+    var idCounter=0;
 	var div1 = null;
 	var div2 = null;
 	var div3 = null;
@@ -25,8 +26,11 @@ window.onload = function(){
 	var div6 = null;
 	var div7 = null;
 	var div8 = null;
+	var p = null;
     var paragr = document.getElementById("count-comments");//.removeChild(div6);
-
+    var messageBirth;
+    
+    console.log(messages);
     
 
 	
@@ -56,118 +60,194 @@ window.onload = function(){
 		return;
 		}
 		
-		message.setDate(now);  //= now;
-		console.log(message);
+		message.setNewDate(now);  //= now;
 		messages.push(message);
-
+		idCounter++;
 		createElements(message);
-		console.log(messages.length);
-		console.log(messages);
 		document.getElementById("comment-box").value = null;
 
-		return messages.length;
+		//return messages.push(message);
 
     }
-    
+
     function createElements(message){
 	    
-        div6 = document.getElementById("count-length");
-        paragr.removeChild(div6);
-	    
+        //div6 = document.getElementById("count-length");
+        //paragr.removeChild(div6);
+        div6 = document.getElementsByClassName("count-length");
+	    while(div6[0]) {
+         div6[0].parentNode.removeChild(div6[0]);
+        }
 	    
 	    li = document.createElement("li");
     	document.getElementById("comment-holder").appendChild(li);
-        
-        li.innerHTML = message.message;
-	    li.id = "comment-holder-li";
+    	
+    	//li.innerHTML = message.message;//toString();//message.message;
+        li.className ="comment-holder-li";
+	    //li.id = "comment-holder-li";
+	    li.id = idCounter+"li";
+
 	    ul.appendChild(li);
+    	
+        p=document.createElement("p");
+        document.getElementById(li.id).appendChild(p);
+        p.className ="comment-paragraf";
+        li.appendChild(p);
+        p.innerHTML = message.message;
+
+
+        
+        
 	    
 	    
 	    div1 = document.createElement("div");
-        document.getElementById("comment-holder-li").appendChild(div1);
-        div1.id = "comment-upp-buttons";
+        document.getElementById(li.id).appendChild(div1);
+        //document.getElementsByClassName("comment-holder-li")[0];
+        div1.className = "comment-upp-buttons";
+        div1.id = idCounter+"comment-upp-buttons";
         div1.innerHTML;
         li.appendChild(div1);
         
         div2 = document.createElement("ul");
-        document.getElementById("comment-upp-buttons").appendChild(div2);
-        div2.id = "div2";
+        document.getElementById(div1.id).appendChild(div2);
+        //document.getElementsByClassName("comment-upp-buttons")[0];
+        div2.id = idCounter+"ul2";
         div2.innerHTML;
         div1.appendChild(div2);
         
         div3 = document.createElement("li");
-        document.getElementById("div2").appendChild(div3);
-        div3.id = "div3";
+        document.getElementById(div2.id).appendChild(div3);
+        div3.id = idCounter+"li2";
         div3.innerHTML;
         div2.appendChild(div3);
         
         div4 = document.createElement("img");
-        document.getElementById("div3").appendChild(div4);
-        div4.id = "delete-button";
+        document.getElementById(div3.id).appendChild(div4);
+        div4.id = idCounter+"date-button";
+        div4.className = "date-button";
         div4.innerHTML;
         div3.appendChild(div4);
         div4.setAttribute("src", "pics/TIMER.png");
+        div4.setAttribute("onClick", "");
+        //div4.onclick = showDate;
+        div4.addEventListener("click", showDate);
         
         div5 = document.createElement("img");
-        document.getElementById("delete-button").appendChild(div5);
-        div5.id = "date-button";
+        document.getElementById(div4.id).appendChild(div5);
+        div5.className ="delete-button";
+        div5.id =idCounter+ "delete-button";
         div3.appendChild(div5);
         div5.innerHTML;
         div5.setAttribute("src", "pics/DELETE.png");
-        
+        div5.setAttribute("onClick", "");
+        //div5.onclick = deleteMessage;
+        div5.addEventListener("click", deleteMessage);
+
         
         div8 = document.createElement("div");
-        document.getElementById("comment-holder-li").appendChild(div8);
+        document.getElementsByClassName(li.id)[1];//.appendChild(div8);
         div5.appendChild(div8);
-        div8.id = "comment-date";
+        div8.id = idCounter+"comment-date";
+        div8.className ="comment-date";
         li.appendChild(div8);
 
         div7 = document.createElement("code");
-        document.getElementById("comment-date").appendChild(div7);
-        div7.id = "writen-date";
+        document.getElementById(div8.id).appendChild(div7);
+        div7.className = "writen-date";
+        div7.id = idCounter+"writen-date";
         div8.appendChild(div7);
-        div7.innerHTML= message.date;
+        div7.innerHTML= showTime(message.date);
 
 
         div6 = document.createElement("code");
         document.getElementById("count-comments").appendChild(div6);
-        div6.id = "count-length";
+        div6.id = idCounter+"count-length";
+        div6.className = "count-length";
         paragr.appendChild(div6);
 
         div6.innerHTML= messages.length;
         
-
-	    //return li;
-	}
-
-    function Message (message, date){
-        
-        this.message;
-        this.date;
     }
         
-    Message.prototype.getText = function(){
-        return this.message;
-    };
-    
-    Message.prototype.setText = function(newText){
+    function showTime(str){
         
-        if(typeof newText ==="undefined" || newText.trim() ===""){
-            
-            console.log("Textarean är tom!");
+        
+        str  = message.date;
+		
+		//Create a new date-object to find the exact day. 
+	    messageBirth = new Date((-(-str - now))-now ); 
+        
+        var hours = messageBirth.getHours();
+        var minutes = messageBirth.getMinutes();
+        var seconds = messageBirth.getSeconds();
+
+        //messageBirth.getHours()+":"+messageBirth.getMinutes()+":"+messageBirth.getSeconds()
+        if(hours < 10){
+            hours ="0"+hours;
         }
-        
-        this.message = newText; 
-    };
+        if (minutes < 10){
+            minutes="0"+minutes;
+        }
+        if(seconds < 10){
+            seconds ="0"+seconds;
+        }
+        var time = hours+":"+minutes+":"+seconds;
+        return time; 
+
+    }
     
-     Message.prototype.getDate = function(){
-        return this.date;
-    };
-    Message.prototype.setDate = function(newDate){
+    function showDate(str){
         
-       this.date = newDate; 
-    };
+         str  = message.date;
+         var timeNew = showTime(str); 
+
+        //Create two Arrays with Swedish names for days and months!
+		var dayName=["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fridag", "Lördag"];
+		var monthName=["Jenuari", "Februari", "Mars","April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"];
+		
+		
+		//Create a new date-object to find the exact day. 
+	    messageBirth= new Date((-(-str - now))-now );
+	    
+	    var dateCreation = dayName[messageBirth.getDay()] +" "+messageBirth.getDate()+ " "+monthName[messageBirth.getMonth()]+" "+messageBirth.getFullYear() +"   ";
+		//Precentate date and time 
+        var str1 = "Meddelandet skapades " +dateCreation+timeNew;
+        alert(str1);
+    }
     
+   function deleteMessage(str){
+       
+      
+        //var a = str.target.nodeName;
+        var a = str.target.id;
+        var b =a.replace('delete-button','');
+        var c = b +"li";
+    
+        var conf = confirm("Vill du verkligen radera meddelandet?");
+         
+        div6.innerHTML= --messages.length;
+
+        var x = document.getElementById(c);
+        x.remove(x.selectedIndex);
+        
+       
+
+        
+        //x.remove(x.selectedIndex);
+        
+        /*var b = document.getElementById(x).parentNode.id;
+        var c = document.getElementById(b).parentNode.id;
+        var d = document.getElementById(c).parentNode.nodeName;
+        var e = document.getElementById(d).parentNode.id;
+        //alert(event.target.id+" and "+event.target.class);
+         
+        
+        //console.log(c);*/
+       
+   }
 
 };
+	
 
+
+   
