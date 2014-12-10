999ac03d89a54803774f1e74e9df08cd00bf83d3
dn222em <dn222em@student.lnu.se>
2014-12-10 11:14:52 +0000

    game on

---
+++ 3-gameon/memory/script/MemoryBoard.js
@@ -0,0 +1,15 @@
+"use strict";
+/* I denna fil körs vår applikation.
+ * Vi använder window.onload för att starta applikationen när
+ * fönstret laddat klart. * 
+ */
+var MemoryBoard = {
+    
+    init : function (){
+        console.log("hello");
+        
+    }
+    
+};
+
+window.onload = MemoryBoard.init;
