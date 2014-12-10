7e09446ab1bfb0f7906441de80d14f94b45e8fc0
dn222em <dn222em@student.lnu.se>
2014-12-10 13:18:37 +0000

    a

--- 3-gameon/memory/script/MemoryBoard.js
+++ 3-gameon/memory/script/MemoryBoard.js
@@ -5,8 +5,78 @@
  */
 var MemoryBoard = {
     
+    count : 0,
+    
     init : function (){
+        
+        ++MemoryBoard.count;
+
+        var defaultPictures = [];
+        var picture=null;
+        var memoryBoard=null;
+        
+        pushElements(defaultPictures);
+        
+        function pushElements(defaultPictures){
+            for (var i =0; i<16; i++){
+                picture = document.createElement("img");
+                memoryBoard = document.getElementById("memoryBoard");
+                document.getElementById("memoryBoard").appendChild(picture);
+                picture.setAttribute("src", "memory/pics/0.png");
+                picture.setAttribute("onClick", "");
+    
+                picture.addEventListener("click", showImage);
+
+
+                picture.id="picture No"+ MemoryBoard.count;
+                picture.className="defaultPicture";
+                MemoryBoard.count++;
+                picture.innerHTML; 
+                memoryBoard.appendChild(picture);
+                defaultPictures.push(picture);
+               // console.log(defaultPictures);
+
+            }
+        }
+        function showImage(str){
+            var a = str.target.id;
+            var element = document.getElementById(a);
+            element.setAttribute("src", "memory/pics/1.png");
+            
+            console.log(a);
+            return element.innerHTML;
+
+            /*picture = document.createElement("img");
+            memoryBoard = document.getElementById("memoryBoard");
+            document.getElementById("memoryBoard").appendChild(picture);
+            picture.setAttribute("src", "memory/pics/1.png");
+
+            picture.id="picture No"+ MemoryBoard.count;
+            picture.className="defaultPicture";
+            MemoryBoard.count++;
+            picture.innerHTML; 
+            memoryBoard.appendChild(picture);*/
+        }
+        
+
+        /*var memoryBoard = document.getElementById("memoryBoard");
+
+       
+        //memoryBoard.appendChild(picture);
+        
+      
+        var gameBoard = document.createElement("div");
+        document.getElementById("memoryBoard").appendChild(gameBoard);
+        gameBoard.innerHTML = pushElements();
+        memoryBoard.appendChild(gameBoard);*/
+        
+        
+       // = url("../pics/0.png");
         console.log("hello");
+                console.log(defaultPictures);
+                                console.log();
+
+
         
     }
     
